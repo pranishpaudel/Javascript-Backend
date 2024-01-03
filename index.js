c88021445ef5c06b7c08f88f1255a9969ef9131a
Pranish <pranishpoudel01@gmail.com>
2024-01-03 16:05:22 +0545

    feat: :sparkles: Express Js starting

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import 'dotenv/config'
+
+
+const app =express();
+
+app.get('/',(req,res)=>{
+    res.send('Hello world!')
+})
+
+app.get('/pranish',(req,res)=>{
+    res.send('Hello world tweet!')
+    console.log(req.body)
+})
+
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Listening on port ${process.env.PORT}`);
+});
